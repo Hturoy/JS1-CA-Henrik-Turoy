8c3636ddb69c7eccbb1a538bf80698612c0323f2
Hturoy <69842773+Hturoy@users.noreply.github.com>
2021-02-23 16:13:03 +0100

    jsjsj

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("Is script linked? yes")
+
+const content = document.querySelector (".theDiv")
+const detailContent = document.querySelector(".detailsDiv")
+const url = "https://rawg-video-games-database.p.rapidapi.com/games?page=3"
+// https://rawg-video-games-database.p.rapidapi.com/games/4332 For å få tak i ett spill med ID
+
+
+
+
+listGames = (games) => {
+	content.innerHTML = "";
+	for (let i of games.results) {
+		console.log(i)
+		if (i === games.results[5]){
+			break
+		}
+	let newContent = `
+	<a href="./details.html"><ul>
+		<h2>${i.name}</h2>
+		<p>Metacritic Score: ${i.metacritic}</p>
+		<p>Ratings: ${i.ratings_count}</p>
+		<p>Reviews: ${i.reviews_count}</p>
+	</ul></a>`;
+		content.innerHTML += newContent;
+		
+	}
+}
+
+fetch(url, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "3713bb56d2msh095eea4b2b371d6p122884jsne8dd845e6aa5",
+		"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com"
+	}
+})
+
+.then(response => response.json())
+.then(data => listGames(data))
+.catch(error =>{
+	console.error(error.message);
+	content.innerHTML = `<div class="error">This Does Not Woooork</div>`
+})
+
+
+
